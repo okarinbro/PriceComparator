f800fd8a4f4c40c81330dc68c84b0f95a0269177
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-23 16:24:38 +0200

    Handle message when no price found

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -4,6 +4,7 @@ class Client(server: ActorRef) extends Actor {
   override def receive(): Receive = {
     case productName: String => this.server ! Query(productName); println("client")
     case result: QueryResult => println(result.productName, " ", result.price, " ", result.occurrences)
+    case notFound: PriceNotFound => println("Price for %s not found. Your query occurrences: %d".format(notFound.productName, notFound.occurrences))
   }
 }
 
