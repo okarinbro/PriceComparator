aceac40d39b1030b99647a4718bfdafc8fd22876
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-24 13:54:28 +0200

    Validate input

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,34 @@
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import database.DatabaseInitializer
 
+import scala.collection.mutable
 import scala.io.StdIn.readLine
 
-//todo: strategies one for all or ...
 object Main {
-
   def main(args: Array[String]): Unit = {
     new DatabaseInitializer().initDbIfNotExists()
     implicit val system: ActorSystem = ActorSystem("local_system")
 
     val server = system.actorOf(Props[Server], "server")
-    val client1 = system.actorOf(Client.apply(server), "client1")
-    val client2 = system.actorOf(Client.apply(server), "client2")
+    val clients = new mutable.HashMap[String, ActorRef]()
+    for (i <- 1 to 10) {
+      clients += ("" + i -> system.actorOf(Client.apply(server), "client" + i))
+    }
 
     while (true) {
-      println("Type product name")
-      val productName = readLine()
-      client1 ! productName
+      println("Type [client number from 1 to 10]:[product name]. To exit: 'q'")
+      val command = readLine()
+      if (command.equals("q")) {
+        System.exit(0)
+      }
+      if (!"[1-10]:.+".r.matches(command)) {
+        println("Invalid input")
+      } else {
+        val params = command.split(":")
+        val actorNumber = params.head
+        val productName = params.tail.mkString(" ")
+        clients(actorNumber) ! productName
+      }
     }
   }
 }
